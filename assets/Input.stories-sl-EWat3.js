30324ee6b771b06abe768b3052f103a808786c50
Igor Deriabin <igor@deriab.in>
2024-09-29 21:03:11 +0300

    add UI kit

---
+++ assets/Input.stories-sl-EWat3.js
@@ -0,0 +1,10 @@
+import{j as e}from"./jsx-runtime-CkxqCPlQ.js";import{I as p}from"./Input-BH_k2DoX.js";import"./index-DJO9vBfz.js";const x={title:"UI/Components/Input",component:p,tags:["autodocs"],decorators:u=>e.jsx("div",{className:"max-w-[1280px]",children:e.jsx(u,{})})},r={args:{value:"SmartyTutor"}},a={args:{value:"SmartyTutor",label:"Введи текст"}};var t,s,o;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    value: 'SmartyTutor'
+  }
+}`,...(o=(s=r.parameters)==null?void 0:s.docs)==null?void 0:o.source}}};var m,n,c;a.parameters={...a.parameters,docs:{...(m=a.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    value: 'SmartyTutor',
+    label: 'Введи текст'
+  }
+}`,...(c=(n=a.parameters)==null?void 0:n.docs)==null?void 0:c.source}}};const g=["Default","WithLabel"];export{r as Default,a as WithLabel,g as __namedExportsOrder,x as default};
