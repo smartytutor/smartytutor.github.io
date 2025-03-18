261d67d28175801b26b3a34e52b57dd66dae33ac
Igor Deriabin <igor@deriab.in>
2025-03-18 06:20:33 +0400

    Update Storybook

--- ui/assets/Sign.stories-noU1mzDM.js
+++
@@ -1,5 +0,0 @@
-import{a as p}from"./Sign-M3dRSRBQ.js";import"./jsx-runtime-CkxqCPlQ.js";import"./index-DJO9vBfz.js";import"./clsx-B-dksMZM.js";import"./_isIterateeCall-ghNbYjRD.js";import"./_arrayLikeKeys-BP-wO3FV.js";import"./isArray-CqxlJJN4.js";import"./keysIn-aHi1SaSt.js";const f={title:"TextProblem/Sign",component:p,args:{variant:"add"}},r={},a={args:{size:"small"}};var e,o,s;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:"{}",...(s=(o=r.parameters)==null?void 0:o.docs)==null?void 0:s.source}}};var t,m,i;a.parameters={...a.parameters,docs:{...(t=a.parameters)==null?void 0:t.docs,source:{originalSource:`{
-  args: {
-    size: 'small'
-  }
-}`,...(i=(m=a.parameters)==null?void 0:m.docs)==null?void 0:i.source}}};const z=["Sign","Small"];export{r as Sign,a as Small,z as __namedExportsOrder,f as default};
---
+++ ui/assets/UI.stories-DSvfGrCm.js
@@ -0,0 +1,16 @@
+import{j as r}from"./jsx-runtime-CkxqCPlQ.js";import{A as h}from"./Action-DcFBsC7h.js";import{B as u}from"./Button-BFim8KLb.js";import S from"./Chat.stories-mvVQXxcb.js";import{U as g}from"./UI-DZ-SIrdS.js";import"./index-DJO9vBfz.js";import"./clsx-B-dksMZM.js";import"./Icon-CA65L85m.js";import"./proxy-Bu-Muats.js";import"./Chat-DbB_edh2.js";import"./Answer-BBzPdijR.js";import"./Message-B84R2Yj4.js";import"./redux-toolkit.modern-DEr0YuQG.js";import"./keys-V90hh7H8.js";import"./_arrayLikeKeys-BP-wO3FV.js";import"./isArray-CqxlJJN4.js";import"./_overArg-BLH_OBOE.js";import"./_isIterateeCall-ghNbYjRD.js";import"./Menu-C_SWLeMl.js";import"./ProgressBar-DvhAnra0.js";import"./Smartik-B_fkXt3u.js";const D={title:"UI/Screen",component:g,args:{chat:{messages:S.args.messages},action:r.jsx(h,{position:"bottom",onSubmit:()=>{},children:["Первая","Вторая","Третья"].map(e=>r.jsx(u,{variant:"answer",children:e},e))}),progressBar:{title:"Вводный урок: Вступление",progress:[{rate:0,completed:!1},{rate:0,completed:!1},{rate:0,completed:!1},{rate:0,completed:!1}]},children:r.jsx("img",{src:"/dragon.png"})},tags:["!autodocs"]},s={decorators:[e=>r.jsx("div",{className:"h-[657px] w-[1280px] shadow-2xl",children:r.jsx(e,{})})]},o={decorators:[e=>r.jsx("div",{className:"h-[969px] min-h-[657px] w-[1920px] min-w-[1280px] shadow-2xl",children:r.jsx(e,{})})]},a={parameters:{layout:"fullscreen"},decorators:[e=>r.jsx("div",{className:"h-screen min-h-[657px] w-screen min-w-[1280px]",children:r.jsx(e,{})})]};var t,m,p;s.parameters={...s.parameters,docs:{...(t=s.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  decorators: [Story => <div className='h-[657px] w-[1280px] shadow-2xl'>
+        <Story />
+      </div>]
+}`,...(p=(m=s.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};var n,c,i;o.parameters={...o.parameters,docs:{...(n=o.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  decorators: [Story => <div className='h-[969px] min-h-[657px] w-[1920px] min-w-[1280px] shadow-2xl'>
+        <Story />
+      </div>]
+}`,...(i=(c=o.parameters)==null?void 0:c.docs)==null?void 0:i.source}}};var d,l,x;a.parameters={...a.parameters,docs:{...(d=a.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  parameters: {
+    layout: 'fullscreen'
+  },
+  decorators: [Story => <div className='h-screen min-h-[657px] w-screen min-w-[1280px]'>
+        <Story />
+      </div>]
+}`,...(x=(l=a.parameters)==null?void 0:l.docs)==null?void 0:x.source}}};const F=["SmallScreen","LargeScreen","Responsive"];export{o as LargeScreen,a as Responsive,s as SmallScreen,F as __namedExportsOrder,D as default};
