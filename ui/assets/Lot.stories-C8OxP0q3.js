c554896656efeb0d6b2418e41b985d16d7d384a7
Igor Deriabin <igor@deriab.in>
2025-03-20 14:47:56 +0400

    Update Storybook

--- ui/assets/Lot.stories-C8OxP0q3.js
+++
@@ -1,5 +0,0 @@
-import{j as e}from"./jsx-runtime-CkxqCPlQ.js";import{D as i}from"./Draggable-B7pOjmDM.js";import{L as l}from"./Lot-CzPNOV6v.js";import"./index-DJO9vBfz.js";import"./clsx-B-dksMZM.js";import"./use-animate-fs2rhT79.js";import"./proxy-Bu-Muats.js";const j={title:"AreaTiles/Lot",component:l},r={},o={decorators:n=>e.jsx(i,{children:e.jsx(n,{})})};var a,t,s;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:"{}",...(s=(t=r.parameters)==null?void 0:t.docs)==null?void 0:s.source}}};var m,p,c;o.parameters={...o.parameters,docs:{...(m=o.parameters)==null?void 0:m.docs,source:{originalSource:`{
-  decorators: Story => <DraggableComponent>
-      <Story />
-    </DraggableComponent>
-}`,...(c=(p=o.parameters)==null?void 0:p.docs)==null?void 0:c.source}}};const S=["Default","Draggable"];export{r as Default,o as Draggable,S as __namedExportsOrder,j as default};
---
+++ ui/assets/LotPicker.stories-BXizDziY.js
@@ -0,0 +1,9 @@
+import{L as p}from"./LotPicker-B688_aQh.js";import"./jsx-runtime-CkxqCPlQ.js";import"./index-DJO9vBfz.js";import"./clsx-B-dksMZM.js";import"./context-DDGWZyhU.js";import"./Draggable-B7pOjmDM.js";import"./use-animate-fs2rhT79.js";import"./proxy-Bu-Muats.js";import"./Lot-DD1NbmHj.js";const k={title:"AreaTiles/LotPicker",component:p},r={args:{count:5}},o={args:{count:0}};var t,e,a;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    count: 5
+  }
+}`,...(a=(e=r.parameters)==null?void 0:e.docs)==null?void 0:a.source}}};var s,c,m;o.parameters={...o.parameters,docs:{...(s=o.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    count: 0
+  }
+}`,...(m=(c=o.parameters)==null?void 0:c.docs)==null?void 0:m.source}}};const x=["Default","Empty"];export{r as Default,o as Empty,x as __namedExportsOrder,k as default};
