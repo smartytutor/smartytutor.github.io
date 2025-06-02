a89fe55596b70cfe7e63843362ae77c4f4ac54e7
Igor Deriabin <igor@deriab.in>
2025-06-02 23:55:27 +0400

    Update Storybook

---
+++ ui/assets/DraggableLot.stories-C35BGLG-.js
@@ -0,0 +1,5 @@
+import{T as a}from"./constants-K3C4fZDC.js";import{D as s}from"./DraggableLot-pFxc_x3A.js";import"./jsx-runtime-DR9Q75dM.js";import"./index-DRjF_FHU.js";import"./clsx-B-dksMZM.js";import"./Lot-BNKJq9KC.js";import"./defaults-DdJ9H2pA.js";import"./_arrayLikeKeys-BBTc_PJo.js";import"./proxy-C00yw4cG.js";const f={title:"AreaTiles/DraggableLot",component:s},r={args:{size:a}};var o,t,e;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    size: TILE_SIZE
+  }
+}`,...(e=(t=r.parameters)==null?void 0:t.docs)==null?void 0:e.source}}};const D=["Default"];export{r as Default,D as __namedExportsOrder,f as default};
---
+++ ui/assets/Smartik.stories-CAxbQWlM.js
@@ -0,0 +1,5 @@
+import{j as r}from"./jsx-runtime-DR9Q75dM.js";import{S as m}from"./Smartik-BPRx95dn.js";import"./index-DRjF_FHU.js";import"./clsx-B-dksMZM.js";const l={title:"UI/Smartik",component:m,argTypes:{state:{control:"select",options:["proud","book","result","skate","thinks","thumbsup","welcome","correct","highlight","idle","interest","success","training"]}},decorators:o=>r.jsx("div",{className:"max-w-[1280px]",children:r.jsx(o,{})})},s={args:{className:"h-[280px]"}};var t,e,a;s.parameters={...s.parameters,docs:{...(t=s.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    className: 'h-[280px]'
+  }
+}`,...(a=(e=s.parameters)==null?void 0:e.docs)==null?void 0:a.source}}};const d=["Smartik"];export{s as Smartik,d as __namedExportsOrder,l as default};
