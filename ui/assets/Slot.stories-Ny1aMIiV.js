baf592dfaa3ce19d967a9449176240bd4224598c
Igor Deriabin <igor@deriab.in>
2025-07-15 03:49:40 +0400

    Update Storybook

---
+++ ui/assets/Slot.stories-Ny1aMIiV.js
@@ -0,0 +1,31 @@
+import{j as s}from"./jsx-runtime-DR9Q75dM.js";import{S as f}from"./Solution-CkrVeAZA.js";import"./index-DRjF_FHU.js";import"./Sign-C0BDR2GM.js";import"./clsx-B-dksMZM.js";import"./defaults-BleV3Q12.js";import"./_arrayLikeKeys-BOaknIRz.js";import"./keysIn-Bb7DVGF9.js";import"./redux-toolkit.modern-DEr0YuQG.js";import"./index-BM9kYpov.js";import"./DraggableToken-VOVcS7y6.js";import"./Token-WdqJ7wEV.js";import"./proxy-C00yw4cG.js";import"./Highlighted-6-cdO6i-.js";import"./PopoverInput-RZIFm9hh.js";import"./Action-DQ72ZiUY.js";import"./Button-CGecWSMn.js";import"./Icon-DNOHRIBS.js";import"./Answer-CpHO1qYv.js";import"./Chat-CUlhg49f.js";import"./VoiceoverProvider-i68q2e-2.js";import"./Message-ByLECGpj.js";import"./Input-0sRJfh9S.js";import"./Menu-DuHofHgY.js";import"./ProgressBar-Dhx3fOxm.js";import"./Smartik-Bonj4xt4.js";import"./SideSlide-D7--k3Rr.js";import"./Deck-DCC2Jsd6.js";import"./AnimatedCard-BBj7EPCy.js";import"./NumeralFace-CXEVFjF1.js";const $={title:"Тренажёры/TextProblem/Slot",component:f},r={args:{variant:"operand",accepts:["drop"]}},a={args:{variant:"sign",accepts:["drop"]}},o={args:{variant:"operand",size:"small",accepts:["drop"]}},t={args:{variant:"operand",highlighted:!0,accepts:["drop"]}},e={decorators:E=>s.jsx("div",{className:"mb-40",children:s.jsx(E,{})}),args:{variant:"operand",accepts:["input"]}};var p,i,n;r.parameters={...r.parameters,docs:{...(p=r.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    variant: 'operand',
+    accepts: ['drop']
+  }
+}`,...(n=(i=r.parameters)==null?void 0:i.docs)==null?void 0:n.source}}};var m,c,d;a.parameters={...a.parameters,docs:{...(m=a.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    variant: 'sign',
+    accepts: ['drop']
+  }
+}`,...(d=(c=a.parameters)==null?void 0:c.docs)==null?void 0:d.source}}};var g,l,u;o.parameters={...o.parameters,docs:{...(g=o.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    variant: 'operand',
+    size: 'small',
+    accepts: ['drop']
+  }
+}`,...(u=(l=o.parameters)==null?void 0:l.docs)==null?void 0:u.source}}};var S,v,h;t.parameters={...t.parameters,docs:{...(S=t.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    variant: 'operand',
+    highlighted: true,
+    accepts: ['drop']
+  }
+}`,...(h=(v=t.parameters)==null?void 0:v.docs)==null?void 0:h.source}}};var x,b,j;e.parameters={...e.parameters,docs:{...(x=e.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  decorators: Story => <div className='mb-40'>
+      <Story />
+    </div>,
+  args: {
+    variant: 'operand',
+    accepts: ['input']
+  }
+}`,...(j=(b=e.parameters)==null?void 0:b.docs)==null?void 0:j.source}}};const rr=["Operand","Sign","Small","Highlighted","Editable"];export{e as Editable,t as Highlighted,r as Operand,a as Sign,o as Small,rr as __namedExportsOrder,$ as default};
---
+++ ui/assets/channel-DZtrVQP9.js
@@ -0,0 +1,1 @@
+import{U as a,C as n}from"./ScenarioGraphModal-C4usBm0Z.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
