95db617a3bcfcdc61d26f2dab60036c051104e01
Igor Deriabin <igor@deriab.in>
2025-07-29 02:02:03 +0400

    Update Storybook

---
+++ ui/assets/Smartik.stories-CIIDUQVr.js
@@ -0,0 +1,5 @@
+import{j as s}from"./jsx-runtime-DR9Q75dM.js";import{S as m}from"./Smartik-Bzq12SuL.js";import"./index-DRjF_FHU.js";import"./useModuleAssetPreloader-CvyT3WsR.js";import"./clsx-B-dksMZM.js";const d={title:"UI/Smartik",component:m,argTypes:{state:{control:"select",options:["proud","book","result","skate","thinks","thumbsup","welcome","correct","highlight","idle","interest","success","training"]}},decorators:o=>s.jsx("div",{className:"max-w-[1280px]",children:s.jsx(o,{})})},r={args:{className:"h-[280px]"}};var t,e,a;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    className: 'h-[280px]'
+  }
+}`,...(a=(e=r.parameters)==null?void 0:e.docs)==null?void 0:a.source}}};const u=["Smartik"];export{r as Smartik,u as __namedExportsOrder,d as default};
