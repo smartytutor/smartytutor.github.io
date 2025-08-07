cd8b7c210a0242464b62338dd905e0cbe49bbf3f
Igor Deriabin <igor@deriab.in>
2025-08-07 23:45:42 +0400

    Update Storybook

---
+++ ui/assets/UI.stories-CZmVvyIl.js
@@ -0,0 +1,29 @@
+import{j as r}from"./jsx-runtime-DR9Q75dM.js";import{A as p}from"./Action-DQ72ZiUY.js";import{B as s}from"./Button-CGecWSMn.js";import i from"./Chat.stories-6loUywbm.js";import{a as n}from"./UnsupportedResolution-B-JzYZ4u.js";import"./index-DRjF_FHU.js";import"./clsx-B-dksMZM.js";import"./Icon-DNOHRIBS.js";import"./proxy-C00yw4cG.js";import"./Chat-Do3hn2Pg.js";import"./Answer-CpHO1qYv.js";import"./VoiceoverProvider-i68q2e-2.js";import"./Message-ByLECGpj.js";import"./Input-0sRJfh9S.js";import"./Menu-DuHofHgY.js";import"./ProgressBar-Dhx3fOxm.js";import"./Smartik-Bzq12SuL.js";import"./useModuleAssetPreloader-CvyT3WsR.js";import"./index-TcLXE8f6.js";import"./redux-toolkit.modern-DEr0YuQG.js";import"./_arrayLikeKeys-CNLEhIRO.js";import"./index-D8bzLNKF.js";import"./Slot-DWk7hR4w.js";import"./Lot-BdQIuWxX.js";import"./constants-K3C4fZDC.js";import"./Module-DnAXLbcZ.js";import"./DraggableLot-ByRJV8sO.js";import"./defaults-DIlqC9Zn.js";import"./LotPicker-CDravAKg.js";import"./Stack-obeTg5PL.js";import"./LotPalette-CnW9wLxe.js";import"./DraggableToken-DySDDj_J.js";import"./Sign-CY00udov.js";import"./Token-CYgrhBmq.js";import"./AnimatedCard-C-7VSUqA.js";import"./NumeralFace-CHcbTEsP.js";import"./Deck-C1DXEKQa.js";import"./Highlighted-6-cdO6i-.js";const T={title:"UI/UI",component:n,parameters:{layout:"fullscreen"},tags:["!autodocs"]},t={args:{chat:{messages:i.args.messages},action:r.jsx(p,{position:"bottom",onSubmit:()=>{},children:["Первая","Вторая","Третья"].map(o=>r.jsx(s,{variant:"answer",children:o},o))}),progressBar:{title:"Вводный урок: Вступление",progress:[{rate:0,completed:!1},{rate:0,completed:!1},{rate:0,completed:!1},{rate:0,completed:!1}]},children:r.jsx("img",{src:"/dragon.png"})}};var e,m,a;t.parameters={...t.parameters,docs:{...(e=t.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    chat: {
+      messages: ChatMeta.args.messages
+    },
+    action: <Action position='bottom' onSubmit={() => {}}>
+        {['Первая', 'Вторая', 'Третья'].map(text => <Button variant='answer' key={text}>
+            {text}
+          </Button>)}
+      </Action>,
+    progressBar: {
+      title: 'Вводный урок: Вступление',
+      progress: [{
+        rate: 0,
+        completed: false
+      }, {
+        rate: 0,
+        completed: false
+      }, {
+        rate: 0,
+        completed: false
+      }, {
+        rate: 0,
+        completed: false
+      }]
+    },
+    children: <img src='/dragon.png' />
+  }
+}`,...(a=(m=t.parameters)==null?void 0:m.docs)==null?void 0:a.source}}};const V=["UI"];export{t as UI,V as __namedExportsOrder,T as default};
