90ba1cb2a179b75fc8c0758a6324d889b882e2a1
Igor Deriabin <igor@deriab.in>
2025-08-13 21:40:32 +0000

    Deploy from smartytutor/frontend@9b659d0d1b39f64cae639e8937188c00a3669c8b

--- storybook/assets/Lot.stories-CO7hVFDO.js
+++
@@ -1,5 +0,0 @@
-import{T as s}from"./constants-K3C4fZDC.js";import{L as a}from"./Lot-CjPFm4My.js";import"./jsx-runtime-DR9Q75dM.js";import"./index-DRjF_FHU.js";const i={title:"Тренажёры/AreaTiles/Lot",component:a},r={args:{size:s}};var e,o,t;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
-  args: {
-    size: TILE_SIZE
-  }
-}`,...(t=(o=r.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};const d=["Default"];export{r as Default,d as __namedExportsOrder,i as default};
---
+++ ui/assets/channel-BZO7HInt.js
@@ -0,0 +1,1 @@
+import{U as a,C as n}from"./ScenarioGraphModal-3TdP-NFa.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
