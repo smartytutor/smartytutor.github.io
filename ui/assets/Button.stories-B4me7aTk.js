9ed257d182a014f7192802033bd24df74cc3a456
Igor Deriabin <igor@deriab.in>
2025-08-19 00:33:44 +0000

    Deploy from smartytutor/frontend@5a56865cb43065efc972dc8c7aa0067ffb066c00

---
+++ ui/assets/Button.stories-B4me7aTk.js
@@ -0,0 +1,79 @@
+import{j as e}from"./jsx-runtime-DR9Q75dM.js";import{I as r}from"./Icon-DNOHRIBS.js";import{B as K}from"./Button-COaEaaTn.js";import"./index-DRjF_FHU.js";import"./useAssetPreloader-lqYNbhtK.js";import"./SfxProvider-CEKzBDsh.js";import"./useSfx-CWAGEEaH.js";import"./clsx-B-dksMZM.js";const se={title:"UI/Button",component:K,decorators:J=>e.jsx("div",{className:"max-w-[1280px]",children:e.jsx(J,{})})},s={args:{variant:"answer",children:"Venus"}},a={args:{...s.args,isPressed:!0}},n={args:{variant:"answer",children:e.jsx("span",{className:"text-2xl",children:"34"})}},c={args:{variant:"answer-icon",children:e.jsx(r,{name:"play",size:24})}},o={args:{variant:"answer",children:e.jsxs(e.Fragment,{children:["Нет, движущиеся круги тоже можно",e.jsx("br",{}),"пересчитать"]})}},t={args:{variant:"answer-send",children:e.jsxs(e.Fragment,{children:[e.jsx(r,{name:"arrowDown",size:24,className:"rotate-180"}),"Отправить"]})}},i={args:{variant:"puzzle",children:"Exit"}},m={args:{variant:"cta",className:"w-[400px]",children:e.jsxs(e.Fragment,{children:[e.jsx(r,{name:"check",size:24}),"Я готов",e.jsx("span",{className:"absolute right-4 text-sm opacity-40",children:"Space"})]})}},p={args:{variant:"comics",className:"w-[400px] text-white",children:e.jsxs(e.Fragment,{children:[e.jsx(r,{name:"play",size:24}),e.jsx("span",{className:"opacity-70",children:"Продолжить"}),e.jsx("span",{className:"absolute right-4 text-sm opacity-40",children:"Space"})]})}},l={args:{variant:"menu",children:e.jsxs(e.Fragment,{children:[e.jsx(r,{name:"exit",size:20}),"Выйти"]})}},d={args:{variant:"help",className:"w-[105px]",children:e.jsxs(e.Fragment,{children:[e.jsx(r,{name:"help",size:24}),"Нужна помощь"]})}};var u,h,g;s.parameters={...s.parameters,docs:{...(u=s.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    variant: 'answer',
+    children: 'Venus'
+  }
+}`,...(g=(h=s.parameters)==null?void 0:h.docs)==null?void 0:g.source}}};var x,w,j;a.parameters={...a.parameters,docs:{...(x=a.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  args: {
+    ...AnswerChoiceWord.args,
+    isPressed: true
+  }
+}`,...(j=(w=a.parameters)==null?void 0:w.docs)==null?void 0:j.source}}};var v,z,N;n.parameters={...n.parameters,docs:{...(v=n.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  args: {
+    variant: 'answer',
+    children: <span className='text-2xl'>34</span>
+  }
+}`,...(N=(z=n.parameters)==null?void 0:z.docs)==null?void 0:N.source}}};var S,A,C;c.parameters={...c.parameters,docs:{...(S=c.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    variant: 'answer-icon',
+    children: <Icon name='play' size={24} />
+  }
+}`,...(C=(A=c.parameters)==null?void 0:A.docs)==null?void 0:C.source}}};var I,y,b;o.parameters={...o.parameters,docs:{...(I=o.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  args: {
+    variant: 'answer',
+    children: <>
+        Нет, движущиеся круги тоже можно
+        <br />
+        пересчитать
+      </>
+  }
+}`,...(b=(y=o.parameters)==null?void 0:y.docs)==null?void 0:b.source}}};var F,P,f;t.parameters={...t.parameters,docs:{...(F=t.parameters)==null?void 0:F.docs,source:{originalSource:`{
+  args: {
+    variant: 'answer-send',
+    children: <>
+        <Icon name='arrowDown' size={24} className='rotate-180' />
+        Отправить
+      </>
+  }
+}`,...(f=(P=t.parameters)==null?void 0:P.docs)==null?void 0:f.source}}};var E,B,W;i.parameters={...i.parameters,docs:{...(E=i.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  args: {
+    variant: 'puzzle',
+    children: 'Exit'
+  }
+}`,...(W=(B=i.parameters)==null?void 0:B.docs)==null?void 0:W.source}}};var k,D,H;m.parameters={...m.parameters,docs:{...(k=m.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  args: {
+    variant: 'cta',
+    className: 'w-[400px]',
+    children: <>
+        <Icon name='check' size={24} />Я готов
+        <span className='absolute right-4 text-sm opacity-40'>Space</span>
+      </>
+  }
+}`,...(H=(D=m.parameters)==null?void 0:D.docs)==null?void 0:H.source}}};var M,T,V;p.parameters={...p.parameters,docs:{...(M=p.parameters)==null?void 0:M.docs,source:{originalSource:`{
+  args: {
+    variant: 'comics',
+    className: 'w-[400px] text-white',
+    children: <>
+        <Icon name='play' size={24} />
+        <span className='opacity-70'>Продолжить</span>
+        <span className='absolute right-4 text-sm opacity-40'>Space</span>
+      </>
+  }
+}`,...(V=(T=p.parameters)==null?void 0:T.docs)==null?void 0:V.source}}};var _,O,R;l.parameters={...l.parameters,docs:{...(_=l.parameters)==null?void 0:_.docs,source:{originalSource:`{
+  args: {
+    variant: 'menu',
+    children: <>
+        <Icon name='exit' size={20} />
+        Выйти
+      </>
+  }
+}`,...(R=(O=l.parameters)==null?void 0:O.docs)==null?void 0:R.source}}};var U,q,G;d.parameters={...d.parameters,docs:{...(U=d.parameters)==null?void 0:U.docs,source:{originalSource:`{
+  args: {
+    variant: 'help',
+    className: 'w-[105px]',
+    children: <>
+        <Icon name='help' size={24} />
+        Нужна помощь
+      </>
+  }
+}`,...(G=(q=d.parameters)==null?void 0:q.docs)==null?void 0:G.source}}};const ae=["AnswerChoiceWord","Pressed","AnswerChoiceNumber","AnswerChoiceIcon","AnswerChoiceSentence","AnswerSend","Puzzle","CTA","Comics","Menu","Help"];export{c as AnswerChoiceIcon,n as AnswerChoiceNumber,o as AnswerChoiceSentence,s as AnswerChoiceWord,t as AnswerSend,m as CTA,p as Comics,d as Help,l as Menu,a as Pressed,i as Puzzle,ae as __namedExportsOrder,se as default};
---
+++ ui/assets/UI.stories-BeFxndPv.js
@@ -0,0 +1,29 @@
+import{j as r}from"./jsx-runtime-DR9Q75dM.js";import{A as a}from"./Action-Dr7HXP1S.js";import{B as i}from"./Button-COaEaaTn.js";import s from"./Chat.stories-kjw7MI34.js";import{a as n}from"./UnsupportedResolution-Dzz1vmqY.js";import"./index-DRjF_FHU.js";import"./clsx-B-dksMZM.js";import"./Icon-DNOHRIBS.js";import"./proxy-C00yw4cG.js";import"./useAssetPreloader-lqYNbhtK.js";import"./SfxProvider-CEKzBDsh.js";import"./useSfx-CWAGEEaH.js";import"./Chat-DTiLBGtG.js";import"./Answer-CpHO1qYv.js";import"./VoiceoverProvider-K0B6OSPK.js";import"./Message-ByLECGpj.js";import"./Input-DYfE1iEf.js";import"./Menu-C2_1i_vX.js";import"./ProgressBar-Dhx3fOxm.js";import"./Smartik-CKzJSXjA.js";import"./useModuleAssetPreloader-BiPJ55Vk.js";import"./index-TcLXE8f6.js";import"./redux-toolkit.modern-DEr0YuQG.js";import"./_arrayLikeKeys-CNLEhIRO.js";import"./index-D8bzLNKF.js";import"./Slot-DWk7hR4w.js";import"./Lot-BdQIuWxX.js";import"./constants-K3C4fZDC.js";import"./Module-DnAXLbcZ.js";import"./DraggableLot-ByRJV8sO.js";import"./defaults-DIlqC9Zn.js";import"./LotPicker-CDravAKg.js";import"./Stack-obeTg5PL.js";import"./LotPalette-CnW9wLxe.js";import"./DraggableToken-CtfOR1bx.js";import"./Sign-CY00udov.js";import"./Token-CYgrhBmq.js";import"./AnimatedCard-BBdIZklt.js";import"./NumeralFace-Cbw6Io6W.js";import"./Deck-DK0g6_yI.js";import"./Highlighted-6-cdO6i-.js";const X={title:"UI/UI",component:n,parameters:{layout:"fullscreen"},tags:["!autodocs"]},t={args:{chat:{messages:s.args.messages},action:r.jsx(a,{position:"bottom",onSubmit:()=>{},children:["Первая","Вторая","Третья"].map(o=>r.jsx(i,{variant:"answer",children:o},o))}),progressBar:{title:"Вводный урок: Вступление",progress:[{rate:0,completed:!1},{rate:0,completed:!1},{rate:0,completed:!1},{rate:0,completed:!1}]},children:r.jsx("img",{src:"/dragon.png"})}};var e,m,p;t.parameters={...t.parameters,docs:{...(e=t.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    chat: {
+      messages: ChatMeta.args.messages
+    },
+    action: <Action position='bottom' onSubmit={() => {}}>
+        {['Первая', 'Вторая', 'Третья'].map(text => <Button variant='answer' key={text}>
+            {text}
+          </Button>)}
+      </Action>,
+    progressBar: {
+      title: 'Вводный урок: Вступление',
+      progress: [{
+        rate: 0,
+        completed: false
+      }, {
+        rate: 0,
+        completed: false
+      }, {
+        rate: 0,
+        completed: false
+      }, {
+        rate: 0,
+        completed: false
+      }]
+    },
+    children: <img src='/dragon.png' />
+  }
+}`,...(p=(m=t.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};const Y=["UI"];export{t as UI,Y as __namedExportsOrder,X as default};
