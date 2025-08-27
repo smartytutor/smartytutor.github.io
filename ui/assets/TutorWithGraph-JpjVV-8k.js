d45377e4f7038ef0ebdb244248e6f417743bec42
Igor Deriabin <igor@deriab.in>
2025-08-27 07:43:38 +0000

    Deploy from smartytutor/frontend@25fd788cdd86360465ce9e63dd2516798c149fbf

---
+++ ui/assets/TutorWithGraph-JpjVV-8k.js
@@ -0,0 +1,32 @@
+import{j as e}from"./jsx-runtime-DiklIkkE.js";import{f as t}from"./UnsupportedResolution-DigRJ6AX.js";import{S as a}from"./ScenarioGraphModal-BFeEnXQy.js";const v=({scenario:n})=>e.jsxs(e.Fragment,{children:[e.jsx(t,{scenario:n}),e.jsx(a,{scenario:n})]});v.__docgenInfo={description:`Утилитарный компонент, который объединяет Tutor с модальным окном графа
+Используется в Storybook stories для удобного тестирования секций сценариев`,methods:[],displayName:"TutorWithGraph",props:{scenario:{required:!0,tsType:{name:"Record",elements:[{name:"union",raw:"'begin' | string",elements:[{name:"literal",value:"'begin'"},{name:"string"}]},{name:"union",raw:`| ContextEvent
+| ProgressEvent
+| ProgressSwitchEvent
+| EndEvent
+| MessageEvent
+| ActionEvent
+| ResetActionEvent
+| InputEvent
+| SelectEvent
+| InteractiveAttachEvent
+| InteractiveDetachEvent
+| InteractiveTriggersEvent
+| InteractiveActionEvent
+| AddWatcherEvent
+| RemoveWatcherEvent
+| ForkEvent
+| StylesEvent
+| AddIdleEvent
+| RemoveIdleEvent
+| HelpEvent
+| ResetHelpEvent
+| ConfettiEvent
+| PinMessageEvent
+| UnpinMessageEvent
+| MemeEvent
+| ComicsEvent
+| StickerEvent
+| ShowSideSlide
+| HideSideSlide
+| PlaySfxEvent
+| StopSfxEvent`,elements:[{name:"ContextEvent"},{name:"ProgressEvent"},{name:"ProgressSwitchEvent"},{name:"EndEvent"},{name:"MessageEvent"},{name:"ActionEvent"},{name:"ResetActionEvent"},{name:"InputEvent"},{name:"SelectEvent"},{name:"InteractiveAttachEvent"},{name:"InteractiveDetachEvent"},{name:"InteractiveTriggersEvent"},{name:"InteractiveActionEvent"},{name:"AddWatcherEvent"},{name:"RemoveWatcherEvent"},{name:"ForkEvent"},{name:"StylesEvent"},{name:"AddIdleEvent"},{name:"RemoveIdleEvent"},{name:"HelpEvent"},{name:"ResetHelpEvent"},{name:"ConfettiEvent"},{name:"PinMessageEvent"},{name:"UnpinMessageEvent"},{name:"MemeEvent"},{name:"ComicsEvent"},{name:"StickerEvent"},{name:"ShowSideSlide"},{name:"HideSideSlide"},{name:"PlaySfxEvent"},{name:"StopSfxEvent"}]}],raw:"Record<EventId, ScenarioEvent>"},description:""}}};export{v as T};
---
+++ ui/assets/clone-aulpIFhC.js
@@ -0,0 +1,1 @@
+import{b as r}from"./_baseUniq-M3cL0-2Y.js";var e=4;function a(o){return r(o,e)}export{a as c};
