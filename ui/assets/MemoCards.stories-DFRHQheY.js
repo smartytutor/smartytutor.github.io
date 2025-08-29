c015c7a1ee914377cdd659354721a0ca738c4de1
Igor Deriabin <igor@deriab.in>
2025-08-29 03:58:22 +0000

    Deploy from smartytutor/frontend@fc238fe5bfd5b4eb0f8b7218c968ca9ce6d85a75

---
+++ ui/assets/MemoCards.stories-DFRHQheY.js
@@ -0,0 +1,387 @@
+import{j as c}from"./jsx-runtime-DiklIkkE.js";import"./Action-DUkQC_DA.js";import"./Answer-PL8H0T0r.js";import"./Button-BGi-_Oof.js";import"./Chat--PJB7fTa.js";import"./Icon-C_N4S_ZJ.js";import"./Input-DcyMQBIt.js";import"./index-DRjF_FHU.js";import"./Menu-DjtdNAg8.js";import"./Message-BVAFNPDt.js";import"./ProgressBar-DX19bTZY.js";import"./Smartik-DnMQQPug.js";import{M as S,a as y}from"./UnsupportedResolution-BYvxTsT7.js";import{UI as I}from"./UI.stories-x13e_jh3.js";import{a as h}from"./AnimatedCard-CCJgi9RF.js";import"./clsx-B-dksMZM.js";import"./proxy-BjE8Ig1W.js";import"./useAssetPreloader-lqYNbhtK.js";import"./SfxProvider-C05We5iG.js";import"./useSfx-BACfETso.js";import"./VoiceoverProvider-CSBJPItj.js";import"./index-DUZq2kSe.js";import"./redux-toolkit.modern-CeOX0_sF.js";import"./_arrayLikeKeys-BQo_jNKa.js";import"./index-0xIPAiCx.js";import"./NumeralFace-Ds693XiA.js";import"./Slot-CT1AWMYi.js";import"./Lot-DQsF8ln-.js";import"./constants-K3C4fZDC.js";import"./Module-BbZk3Jp-.js";import"./DraggableLot-BAYEXKcw.js";import"./defaults-mPm_OiqA.js";import"./LotPicker-BH7KDlAi.js";import"./Stack-RW0OyjXV.js";import"./LotPalette-CMrf6JxE.js";import"./DraggableToken-B8Igi4VB.js";import"./Sign-DADmf3_9.js";import"./Token-DuPhmN9g.js";import"./Deck-ngmnPlfX.js";import"./Highlighted-Z1bvx3uf.js";import"./Chat.stories-Bzd2E8N-.js";delete h.faceUp.scale;const{chat:_,progressBar:B}=I.args,Ue={title:"Тренажёры/MemoCards/MemoCards",component:S,tags:["!autodocs"],args:{onTrigger:()=>{}},decorators:[D=>c.jsx(y,{chat:_,progressBar:B,children:c.jsx(D,{})})],parameters:{layout:"fullscreen"}},e={parameters:{store:{MemoCards:{cards:[{status:"decked"},{status:"decked"},{status:"decked"},{status:"decked"},{status:"decked"},{status:"decked"},{status:"decked"},{status:"decked"}]}}}},t={parameters:{store:{MemoCards:{cards:[{status:"decked"},{status:"decked"},{status:"decked"},{status:"decked"},{status:"decked"},{status:"decked"},{status:"decked"},{status:"decked"},{status:"served"},{status:"served"},{status:"served"},{status:"served"},{status:"served"},{status:"served"},{status:"served"},{status:"served"}]}}}},n={parameters:{store:{MemoCards:{cards:[{status:"decked"},{status:"decked"},{status:"decked"},{status:"decked"},{status:"decked"},{status:"decked"},{status:"decked"},{status:"decked"},{status:"served",initial:"faceUp",numeral:{object:"apples",count:13}},{status:"served",initial:"faceUp",numeral:{object:"corn",count:2}},{status:"served",initial:"faceUp",numeral:{object:"oranges",count:44}},{status:"served",initial:"faceUp",numeral:{object:"corn",count:21}},{status:"served",initial:"faceUp",numeral:{object:"carrots",count:22}},{status:"served",initial:"faceUp",numeral:{object:"oranges",count:9}},{status:"served",initial:"faceUp",numeral:{object:"apples",count:154}},{status:"served",initial:"faceUp",numeral:{object:"carrots",count:3}}]}}}},s={parameters:{store:{MemoCards:{cards:[{status:"decked"},{status:"decked"},{status:"decked"},{status:"decked"},{status:"decked"},{status:"decked"},{status:"decked"},{status:"decked"},{status:"served",initial:"faceUp",numeral:{object:"apples",count:13}},{status:"served",initial:["faceUp","elevated"],elevated:!0,numeral:{object:"corn",count:2}},{status:"served",initial:"faceUp",numeral:{object:"oranges",count:44}},{status:"served",initial:"faceUp",numeral:{object:"corn",count:21}},{status:"served",initial:"faceUp",numeral:{object:"carrots",count:22}},{status:"served",initial:"faceUp",numeral:{object:"oranges",count:9}},{status:"served",initial:["faceUp","elevated"],elevated:!0,numeral:{object:"apples",count:154}},{status:"served",initial:"faceUp",numeral:{object:"carrots",count:3}}]}}}},a={parameters:{store:{MemoCards:{cards:[{status:"decked"},{status:"decked"},{status:"decked"},{status:"decked"},{status:"decked"},{status:"decked"},{status:"decked"},{status:"decked"},{status:"served",initial:"faceUp",numeral:{object:"apples",count:13}},{status:"selected",initial:["faceUp","selected"],correct:!0,numeral:{object:"corn",count:2}},{status:"served",initial:"faceUp",numeral:{object:"oranges",count:44}},{status:"served",initial:"faceUp",numeral:{object:"corn",count:21}},{status:"selected",initial:["faceUp","selected"],correct:!0,numeral:{object:"carrots",count:22}},{status:"served",initial:"faceUp",numeral:{object:"oranges",count:9}},{status:"served",initial:"faceUp",numeral:{object:"apples",count:154}},{status:"selected",initial:["faceUp","selected"],correct:!1,numeral:{object:"carrots",count:3}}]}}}},r={parameters:{store:{MemoCards:{cards:[{status:"decked"},{status:"decked"},{status:"decked"},{status:"decked"},{status:"decked"},{status:"decked"},{status:"decked"},{status:"decked"},{status:"served",initial:"faceUp",numeral:{object:"apples",count:13}},{status:"discarded",numeral:{object:"corn",count:2}},{status:"served",initial:"faceUp",numeral:{object:"oranges",count:44}},{status:"served",initial:"faceUp",numeral:{object:"corn",count:21}},{status:"discarded",numeral:{object:"carrots",count:22}},{status:"served",initial:"faceUp",numeral:{object:"oranges",count:9}},{status:"served",initial:"faceUp",numeral:{object:"apples",count:154}},{status:"discarded",numeral:{object:"carrots",count:3}}]}}}};var d,u,o;e.parameters={...e.parameters,docs:{...(d=e.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  parameters: {
+    store: {
+      MemoCards: {
+        cards: [{
+          status: 'decked'
+        }, {
+          status: 'decked'
+        }, {
+          status: 'decked'
+        }, {
+          status: 'decked'
+        }, {
+          status: 'decked'
+        }, {
+          status: 'decked'
+        }, {
+          status: 'decked'
+        }, {
+          status: 'decked'
+        }]
+      }
+    }
+  }
+}`,...(o=(u=e.parameters)==null?void 0:u.docs)==null?void 0:o.source}}};var i,l,p;t.parameters={...t.parameters,docs:{...(i=t.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  parameters: {
+    store: {
+      MemoCards: {
+        cards: [{
+          status: 'decked'
+        }, {
+          status: 'decked'
+        }, {
+          status: 'decked'
+        }, {
+          status: 'decked'
+        }, {
+          status: 'decked'
+        }, {
+          status: 'decked'
+        }, {
+          status: 'decked'
+        }, {
+          status: 'decked'
+        }, {
+          status: 'served'
+        }, {
+          status: 'served'
+        }, {
+          status: 'served'
+        }, {
+          status: 'served'
+        }, {
+          status: 'served'
+        }, {
+          status: 'served'
+        }, {
+          status: 'served'
+        }, {
+          status: 'served'
+        }]
+      }
+    }
+  }
+}`,...(p=(l=t.parameters)==null?void 0:l.docs)==null?void 0:p.source}}};var m,k,v;n.parameters={...n.parameters,docs:{...(m=n.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  parameters: {
+    store: {
+      MemoCards: {
+        cards: [{
+          status: 'decked'
+        }, {
+          status: 'decked'
+        }, {
+          status: 'decked'
+        }, {
+          status: 'decked'
+        }, {
+          status: 'decked'
+        }, {
+          status: 'decked'
+        }, {
+          status: 'decked'
+        }, {
+          status: 'decked'
+        }, {
+          status: 'served',
+          initial: 'faceUp',
+          numeral: {
+            object: 'apples',
+            count: 13
+          }
+        }, {
+          status: 'served',
+          initial: 'faceUp',
+          numeral: {
+            object: 'corn',
+            count: 2
+          }
+        }, {
+          status: 'served',
+          initial: 'faceUp',
+          numeral: {
+            object: 'oranges',
+            count: 44
+          }
+        }, {
+          status: 'served',
+          initial: 'faceUp',
+          numeral: {
+            object: 'corn',
+            count: 21
+          }
+        }, {
+          status: 'served',
+          initial: 'faceUp',
+          numeral: {
+            object: 'carrots',
+            count: 22
+          }
+        }, {
+          status: 'served',
+          initial: 'faceUp',
+          numeral: {
+            object: 'oranges',
+            count: 9
+          }
+        }, {
+          status: 'served',
+          initial: 'faceUp',
+          numeral: {
+            object: 'apples',
+            count: 154
+          }
+        }, {
+          status: 'served',
+          initial: 'faceUp',
+          numeral: {
+            object: 'carrots',
+            count: 3
+          }
+        }]
+      }
+    }
+  }
+}`,...(v=(k=n.parameters)==null?void 0:k.docs)==null?void 0:v.source}}};var j,f,b;s.parameters={...s.parameters,docs:{...(j=s.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  parameters: {
+    store: {
+      MemoCards: {
+        cards: [{
+          status: 'decked'
+        }, {
+          status: 'decked'
+        }, {
+          status: 'decked'
+        }, {
+          status: 'decked'
+        }, {
+          status: 'decked'
+        }, {
+          status: 'decked'
+        }, {
+          status: 'decked'
+        }, {
+          status: 'decked'
+        }, {
+          status: 'served',
+          initial: 'faceUp',
+          numeral: {
+            object: 'apples',
+            count: 13
+          }
+        }, {
+          status: 'served',
+          initial: ['faceUp', 'elevated'],
+          elevated: true,
+          numeral: {
+            object: 'corn',
+            count: 2
+          }
+        }, {
+          status: 'served',
+          initial: 'faceUp',
+          numeral: {
+            object: 'oranges',
+            count: 44
+          }
+        }, {
+          status: 'served',
+          initial: 'faceUp',
+          numeral: {
+            object: 'corn',
+            count: 21
+          }
+        }, {
+          status: 'served',
+          initial: 'faceUp',
+          numeral: {
+            object: 'carrots',
+            count: 22
+          }
+        }, {
+          status: 'served',
+          initial: 'faceUp',
+          numeral: {
+            object: 'oranges',
+            count: 9
+          }
+        }, {
+          status: 'served',
+          initial: ['faceUp', 'elevated'],
+          elevated: true,
+          numeral: {
+            object: 'apples',
+            count: 154
+          }
+        }, {
+          status: 'served',
+          initial: 'faceUp',
+          numeral: {
+            object: 'carrots',
+            count: 3
+          }
+        }]
+      }
+    }
+  }
+}`,...(b=(f=s.parameters)==null?void 0:f.docs)==null?void 0:b.source}}};var U,g,C;a.parameters={...a.parameters,docs:{...(U=a.parameters)==null?void 0:U.docs,source:{originalSource:`{
+  parameters: {
+    store: {
+      MemoCards: {
+        cards: [{
+          status: 'decked'
+        }, {
+          status: 'decked'
+        }, {
+          status: 'decked'
+        }, {
+          status: 'decked'
+        }, {
+          status: 'decked'
+        }, {
+          status: 'decked'
+        }, {
+          status: 'decked'
+        }, {
+          status: 'decked'
+        }, {
+          status: 'served',
+          initial: 'faceUp',
+          numeral: {
+            object: 'apples',
+            count: 13
+          }
+        }, {
+          status: 'selected',
+          initial: ['faceUp', 'selected'],
+          correct: true,
+          numeral: {
+            object: 'corn',
+            count: 2
+          }
+        }, {
+          status: 'served',
+          initial: 'faceUp',
+          numeral: {
+            object: 'oranges',
+            count: 44
+          }
+        }, {
+          status: 'served',
+          initial: 'faceUp',
+          numeral: {
+            object: 'corn',
+            count: 21
+          }
+        }, {
+          status: 'selected',
+          initial: ['faceUp', 'selected'],
+          correct: true,
+          numeral: {
+            object: 'carrots',
+            count: 22
+          }
+        }, {
+          status: 'served',
+          initial: 'faceUp',
+          numeral: {
+            object: 'oranges',
+            count: 9
+          }
+        }, {
+          status: 'served',
+          initial: 'faceUp',
+          numeral: {
+            object: 'apples',
+            count: 154
+          }
+        }, {
+          status: 'selected',
+          initial: ['faceUp', 'selected'],
+          correct: false,
+          numeral: {
+            object: 'carrots',
+            count: 3
+          }
+        }]
+      }
+    }
+  }
+}`,...(C=(g=a.parameters)==null?void 0:g.docs)==null?void 0:C.source}}};var M,E,x;r.parameters={...r.parameters,docs:{...(M=r.parameters)==null?void 0:M.docs,source:{originalSource:`{
+  parameters: {
+    store: {
+      MemoCards: {
+        cards: [{
+          status: 'decked'
+        }, {
+          status: 'decked'
+        }, {
+          status: 'decked'
+        }, {
+          status: 'decked'
+        }, {
+          status: 'decked'
+        }, {
+          status: 'decked'
+        }, {
+          status: 'decked'
+        }, {
+          status: 'decked'
+        }, {
+          status: 'served',
+          initial: 'faceUp',
+          numeral: {
+            object: 'apples',
+            count: 13
+          }
+        }, {
+          status: 'discarded',
+          numeral: {
+            object: 'corn',
+            count: 2
+          }
+        }, {
+          status: 'served',
+          initial: 'faceUp',
+          numeral: {
+            object: 'oranges',
+            count: 44
+          }
+        }, {
+          status: 'served',
+          initial: 'faceUp',
+          numeral: {
+            object: 'corn',
+            count: 21
+          }
+        }, {
+          status: 'discarded',
+          numeral: {
+            object: 'carrots',
+            count: 22
+          }
+        }, {
+          status: 'served',
+          initial: 'faceUp',
+          numeral: {
+            object: 'oranges',
+            count: 9
+          }
+        }, {
+          status: 'served',
+          initial: 'faceUp',
+          numeral: {
+            object: 'apples',
+            count: 154
+          }
+        }, {
+          status: 'discarded',
+          numeral: {
+            object: 'carrots',
+            count: 3
+          }
+        }]
+      }
+    }
+  }
+}`,...(x=(E=r.parameters)==null?void 0:E.docs)==null?void 0:x.source}}};const ge=["Empty","Dealing","Dealt","Elevated","Evaluated","Discarded"];export{t as Dealing,n as Dealt,r as Discarded,s as Elevated,e as Empty,a as Evaluated,ge as __namedExportsOrder,Ue as default};
