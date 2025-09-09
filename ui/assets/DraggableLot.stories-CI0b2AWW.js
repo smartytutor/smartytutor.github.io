8a710c2100c5c5c35e9deabe2fb9f8bbdb2f701f
Igor Deriabin <igor@deriab.in>
2025-09-09 00:54:02 +0000

    Deploy from smartytutor/frontend@a878bc7bd43793fc3cf23004e8fad4e17c0e4dd2

---
+++ ui/assets/DraggableLot.stories-CI0b2AWW.js
@@ -0,0 +1,5 @@
+import{T as a}from"./constants-K3C4fZDC.js";import{D as s}from"./DraggableLot-3dxAohuA.js";import"./jsx-runtime-DiklIkkE.js";import"./index-DRjF_FHU.js";import"./clsx-B-dksMZM.js";import"./Lot-DQsF8ln-.js";import"./defaults-mPm_OiqA.js";import"./_arrayLikeKeys-BQo_jNKa.js";import"./proxy-BjE8Ig1W.js";const f={title:"Тренажёры/AreaTiles/DraggableLot",component:s},r={args:{size:a}};var o,t,e;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    size: TILE_SIZE
+  }
+}`,...(e=(t=r.parameters)==null?void 0:t.docs)==null?void 0:e.source}}};const D=["Default"];export{r as Default,D as __namedExportsOrder,f as default};
---
+++ ui/assets/Smartik.stories-DCE7JKOY.js
@@ -0,0 +1,5 @@
+import{j as s}from"./jsx-runtime-DiklIkkE.js";import{S as i}from"./Smartik-BSwAUB1S.js";import"./index-DRjF_FHU.js";import"./clsx-B-dksMZM.js";const l={title:"UI/Smartik",component:i,argTypes:{state:{control:"select",options:["success","interest","waiting","correct","idle","highlight","training","proud","thinks","result"]}},decorators:o=>s.jsx("div",{className:"max-w-[1280px]",children:s.jsx(o,{})})},r={args:{className:"h-[280px]"}};var t,e,a;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    className: 'h-[280px]'
+  }
+}`,...(a=(e=r.parameters)==null?void 0:e.docs)==null?void 0:a.source}}};const d=["Smartik"];export{r as Smartik,d as __namedExportsOrder,l as default};
