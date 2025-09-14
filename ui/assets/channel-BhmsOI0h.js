a62280ea4fefd785a9e6eab5918d7812a73d33da
Igor Deriabin <igor@deriab.in>
2025-09-14 20:51:15 +0000

    Deploy from smartytutor/frontend@bff1dd1f6795f2acfacd9c4601ea3dbe90a91ad4

--- ui/assets/channel-BhmsOI0h.js
+++
@@ -1,1 +0,0 @@
-import{U as a,C as n}from"./ScenarioGraphModal-DB_x-CAl.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
---
+++ ui/assets/channel-Di5qEsMs.js
@@ -0,0 +1,1 @@
+import{U as a,C as n}from"./ScenarioGraphModal-DWw03mdq.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
