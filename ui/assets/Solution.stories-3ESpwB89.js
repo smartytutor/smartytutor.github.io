4d91b75ef3f21060d22d62ebc8c08bdb9d954248
Igor Deriabin <igor@deriab.in>
2025-09-28 16:54:14 +0000

    Deploy from smartytutor/frontend@b7aa2933ff8b216821312a9a657bf8edf525d63b

---
+++ ui/assets/Solution.stories-3ESpwB89.js
@@ -0,0 +1,27 @@
+import{j as p}from"./jsx-runtime-DiklIkkE.js";import{n as e}from"./redux-toolkit.modern-CeOX0_sF.js";import{c as q}from"./UnsupportedResolution-BmvqmNww.js";import"./index-DRjF_FHU.js";import"./Action-DUkQC_DA.js";import"./clsx-B-dksMZM.js";import"./Button-BGi-_Oof.js";import"./useAssetPreloader-lqYNbhtK.js";import"./SfxProvider-C05We5iG.js";import"./useSfx-BACfETso.js";import"./Icon-C_N4S_ZJ.js";import"./proxy-BjE8Ig1W.js";import"./Answer-PL8H0T0r.js";import"./Chat--PJB7fTa.js";import"./VoiceoverProvider-CSBJPItj.js";import"./Message-BVAFNPDt.js";import"./Input-DcyMQBIt.js";import"./Menu-CmpkZITu.js";import"./ProgressBar-DX19bTZY.js";import"./Smartik-BSwAUB1S.js";import"./index-DfmwTPle.js";import"./_arrayLikeKeys-BQo_jNKa.js";import"./index-0xIPAiCx.js";import"./NumeralFace-Ds693XiA.js";import"./Slot-CT1AWMYi.js";import"./Lot-DQsF8ln-.js";import"./constants-K3C4fZDC.js";import"./Module-BbZk3Jp-.js";import"./DraggableLot-3dxAohuA.js";import"./defaults-mPm_OiqA.js";import"./LotPicker-QeHVEY73.js";import"./Stack-RW0OyjXV.js";import"./LotPalette-CMrf6JxE.js";import"./DraggableToken-B8Igi4VB.js";import"./Sign-DADmf3_9.js";import"./Token-DuPhmN9g.js";import"./AnimatedCard-CCJgi9RF.js";import"./Deck-ngmnPlfX.js";import"./Highlighted-Z1bvx3uf.js";const t={title:"Тренажёры/TextProblem/Solution",component:q,decorators:j=>p.jsx("div",{className:"w-[476px]",children:p.jsx(j,{})}),args:{steps:[{label:"Шаг 1",expression:[{type:"slot",item:{id:e(4),variant:"operand",accepts:["drop"],token:{id:e(4),value:5}}},{type:"slot",item:{id:e(4),variant:"sign",token:{id:e(4),value:"add"},accepts:["drop"]}},{type:"slot",item:{id:e(4),variant:"operand",accepts:["drop"],token:{id:e(4),value:2}}},{type:"sign",item:{variant:"equals"}},{type:"slot",item:{id:e(4),variant:"operand",accepts:["drop"],token:{id:e(4),value:10}}}]},{label:"Шаг 2",expression:[{type:"slot",item:{id:e(4),variant:"operand",accepts:["drop"],token:{id:e(4),value:25}}},{type:"slot",item:{id:e(4),variant:"sign",token:{id:e(4),value:"add"},accepts:["drop"]}},{type:"slot",item:{id:e(4),variant:"operand",accepts:["drop"],token:{id:e(4),value:2}}},{type:"sign",item:{variant:"equals"}},{type:"slot",item:{id:e(4),variant:"operand",accepts:["drop"],token:{id:e(4),value:50}}}]},{label:"Шаг 3",expression:[{type:"slot",item:{id:e(4),variant:"operand",accepts:["drop"],token:{id:e(4),value:10}}},{type:"slot",item:{id:e(4),variant:"sign",token:{id:e(4),value:"add"},accepts:["drop"]}},{type:"slot",item:{id:e(4),variant:"operand",accepts:["drop"],token:{id:e(4),value:50}}},{type:"sign",item:{variant:"equals"}},{type:"slot",item:{id:e(4),variant:"operand",accepts:["drop"],token:{id:e(4),value:60}}}]}],onRedo(){}}},r={args:{activeStepIndex:0}},a={args:{activeStepIndex:1}};var i,n,m,d;const s={args:{activeStepIndex:2,steps:[...t.args.steps.slice(0,-1),{...t.args.steps.at(-1),expression:[...(((i=t.args.steps.at(-1))==null?void 0:i.expression)||[]).slice(0,-1),{...(n=t.args.steps.at(-1))==null?void 0:n.expression.at(-1),item:{...(d=(((m=t.args.steps.at(-1))==null?void 0:m.expression)||[]).at(-1))==null?void 0:d.item,token:void 0}}]}]}},o={args:{activeStepIndex:2}};var c,l,v;r.parameters={...r.parameters,docs:{...(c=r.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    activeStepIndex: 0
+  }
+}`,...(v=(l=r.parameters)==null?void 0:l.docs)==null?void 0:v.source}}};var u,g,x;a.parameters={...a.parameters,docs:{...(u=a.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    activeStepIndex: 1
+  }
+}`,...(x=(g=a.parameters)==null?void 0:g.docs)==null?void 0:x.source}}};var S,y,k;s.parameters={...s.parameters,docs:{...(S=s.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    activeStepIndex: 2,
+    steps: [...meta.args.steps.slice(0, -1), {
+      ...meta.args.steps.at(-1),
+      expression: [...(meta.args.steps.at(-1)?.expression || []).slice(0, -1), {
+        ...meta.args.steps.at(-1)?.expression.at(-1),
+        item: {
+          ...(meta.args.steps.at(-1)?.expression || []).at(-1)?.item,
+          token: undefined
+        }
+      }]
+    }] as StepModel[]
+  }
+}`,...(k=(y=s.parameters)==null?void 0:y.docs)==null?void 0:k.source}}};var I,f,b;o.parameters={...o.parameters,docs:{...(I=o.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  args: {
+    activeStepIndex: 2
+  }
+}`,...(b=(f=o.parameters)==null?void 0:f.docs)==null?void 0:b.source}}};const de=["Step1","Step2","Step3","Answer"];export{o as Answer,r as Step1,a as Step2,s as Step3,de as __namedExportsOrder,t as default};
